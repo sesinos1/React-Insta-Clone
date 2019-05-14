01ba62da25b08a21a7e16e042f98a0c48b1ed7d9
sesinos tesfamariam <sesinostesfamariam@sesinoss-mbp.attlocal.net>
2019-05-14 12:04:06 -0700

    adding alt

--- instagram-app/src/components/PostContainer/Post.js
+++ instagram-app/src/components/PostContainer/Post.js
@@ -4,13 +4,13 @@ const Post = props => {
   return (
     <div>
         <div>
-            <img className='thumbnail' src={props.thumbnailUrl} />
-            <h1>
-                {props.username}
-            </h1>
-        </div>
 
-         <img src={props.imageUrl} />
+            <img className='thumbnail' alt="thumbnail" src={props.thumbnailUrl} />
+            
+            <h1>{props.username}</h1>
+
+        </div>
+            <img src={props.imageUrl} alt="thumbnail" />
     </div>
   )
 }
